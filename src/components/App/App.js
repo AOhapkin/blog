5d06898b070064c1a7dbd4e958cb99d6802035cb
Alexei Okhapkin <alexeyyago@gmail.com>
2024-05-13 16:27:56 +0300

    fix Article Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,10 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import ArticlesList from '../ArticlesList/ArticlesList'
 import ArticlePage from '../ArticlePage/ArticlePage';
+import SignIn from '../SignIn/SignIn'
+import SignUp from '../SignUp/SignUp'
+import ProfileEditor from '../ProfileEditor/ProfileEditor'
+import ArticleEditior from '../ArticleEditior/ArticleEditior'
 
 const App = () => {
   return (
@@ -11,8 +15,11 @@ const App = () => {
         <Route exact path="/" component={ArticlesList}/>
         <Route exact path="/articles" component={ArticlesList}/>
         <Route exact path="/articles/:slug" component={ArticlePage} />
-        {/* <Route exact path="/sign-in" component={SignIn} /> */}
-        {/* <Route exact path="/sign-up" component={SignUp} /> */}
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/sign-up" component={SignUp} />
+        <Route exact path="/profile" component={ProfileEditor} />
+        <Route exact path="/new-article" component={ArticleEditior} />
+        <Route exact path="/articles/:slug/edit" component={ArticleEditior} />
       </Switch>
     </Router>
   )
--- src/components/ArticlePage/ArticlePage.js
+++ src/components/ArticlePage/ArticlePage.js
@@ -1,19 +1,32 @@
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { v1 as uuidv1 } from 'uuid';
 import Markdown from 'react-markdown'
+import { message, Popconfirm } from 'antd'
 
 import classes from './ArticlePage.module.scss'
 import { formatDate } from '../../utilities/utilities';
-import { fetchArticleBySlug } from '../../redux/actions/actionCreators'
+import {
+  fetchArticleBySlug,
+  deleteArticle,
+  likeArticle,
+  unlikeArticle,
+} from '../../redux/actions/actionCreators'
+import ErrorAlert from '../ErrorAlert/ErrorAlert'
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
 
 const ArticlePage = () => {
-  const articleData = useSelector((state) => state.articlesReducers.article)
+  const isLogin = useSelector((state) => state.reducerUser.isLogin)
+  const user = useSelector((state) => state.reducerUser.user.username)
+  const article = useSelector((state) => state.reducerArticles.article)
+  const loading = useSelector((state) => state.reducerArticles.loading)
+  const error = useSelector((state) => state.reducerArticles.error)
+
   const { slug } = useParams()
 
   const dispatch = useDispatch()
-
+  const history = useHistory()
   const {
     author,
     title,
@@ -23,64 +36,107 @@ const ArticlePage = () => {
     description,
     createdAt,
     body,
-  } = articleData
+  } = article
   const { username, image } = author
-  const articleMarkdown = body
+  const markdown = body
+
+  const handleLikeClick = () => {
+    !favorited ? dispatch(likeArticle(slug)) : dispatch(unlikeArticle(slug))
+  }
+
+  const confirmDeletion = () => {
+    message.success('Пост удален')
+    dispatch(deleteArticle(slug))
+    history.push('/articles')
+  }
+  const cancelDeletion = () => {
+    message.error('Отмена удаления поста')
+  }
 
   useEffect(() => {
     dispatch(fetchArticleBySlug(slug))
-  }, [dispatch, slug])
+  }, [dispatch, isLogin, slug])
 
   return (
-    <section className={classes.article}>
-      <div className={classes.article__info_li_item}>
-        <div className={classes.article__info}>
-          <div className={classes.article__box_title}>
-            <h5 className={classes.article__title}>{title}</h5>
-            <div className={classes.article__label_favorited}>
-              <button
-                className={
-                  favorited
-                    ? classes.article__button_like_active
-                    : classes.article__button_like
-                }
-              ></button>
-              <p className={classes.article__counter_like}>{favoritesCount}</p>
+    <>
+      {error ? <ErrorAlert message={error} /> : null}
+      {loading && !error ? <LoadingSpinner /> : null}
+      <section className={classes.article}>
+        <div className={classes.article__header}>
+          <div className={classes.article__info}>
+            <div className={classes.article__title_wrapper}>
+              <h5 className={classes.article__title}>{title}</h5>
+              <div className={classes.article__likes_wrapper}>
+                <button
+                  className={
+                    favorited
+                      ? classes.article__like_button_active
+                      : classes.article__like_button
+                  }
+                  disabled={!isLogin ? true : false}
+                  onClick={() => {
+                    handleLikeClick()
+                  }}
+                ></button>
+                <p className={classes.article__likes_count}>{favoritesCount}</p>
+              </div>
             </div>
+
+            <div className={classes.article__tags_wrapper}>
+              {tagList.map((el) => {
+                return (
+                  <span className={classes.article__tag} key={uuidv1()}>
+                    {el}
+                  </span>
+                )
+              })}{' '}
+            </div>
+
+            <span className={classes.article__description}>{description}</span>
           </div>
 
-          <div className={classes.article__box_tags}>
-            {tagList.map((item) => {
-              return (
-                <p className={classes.article__tag} key={uuidv1()}>
-                  {item}
-                </p>
-              )
-            })}
+          <div className={classes.article__author}>
+            <div className={classes.article__author_wrapper}>
+              <span className={classes.article__author_info}>
+                <p className={classes.article__author_name}>{username}</p>
+                <p className={classes.article__date}>{formatDate(createdAt)}</p>
+              </span>
+              <img
+                className={classes.article__author_image}
+                src={image}
+                alt="user avatar"
+              />
+            </div>
+            {username === user ? (
+              <div className={classes.article__author_controls}>
+                <Popconfirm
+                  placement="rightTop"
+                  description="Are you sure to delete this article?"
+                  onConfirm={confirmDeletion}
+                  onCancel={cancelDeletion}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <button className={classes.article__button_delete} type="submit">
+                    Delete
+                  </button>
+                </Popconfirm>
+                <Link
+                  to={`/articles/${slug}/edit`}
+                  className={classes.article__button_edit}
+                >
+                  Edit
+                </Link>
+              </div>
+            ) : null}
           </div>
-
-          <p className={classes.article__description}>{description}</p>
         </div>
 
-        <div className={classes.article__info_author}>
-          <div className={classes.article__info_author_box}>
-            <span className={classes.article__box_name_date}>
-              <p className={classes.article__author_name}>{username}</p>
-              <p className={classes.article__date}>{formatDate(createdAt)}</p>
-            </span>
-            <img
-              className={classes.article__author_avatar}
-              src={image}
-              alt="user avatar"
-            />
-          </div>
+        <div className={classes.article__content}>
+          <Markdown>{markdown}</Markdown>
         </div>
-      </div>
-
-      <div className={classes.article__info_markdown}>
-        <Markdown>{articleMarkdown}</Markdown>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
